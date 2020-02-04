21bc259f1a87bc9766cb8f3ef9f53fc2667358ea
armanfar <Ramin.Armanfar@msg.group>
2020-02-04 12:58:39 +0100

    Back button added to the actionbar.

--- app/src/main/java/com/raminarmanfar/learngermanapp/HerrProfessorListFragment.kt
+++ app/src/main/java/com/raminarmanfar/learngermanapp/HerrProfessorListFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class HerrProfessorListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? { // Inflate the layout for this fragment
